3f3b8c79ede63954d4716d12fdd741190c5ccefe
Temosho shaku <52781030+TemoshoS@users.noreply.github.com>
2023-02-28 14:17:04 +0200

    Add files via upload

---
+++ javascript/javascript.js
@@ -0,0 +1,86 @@
+
+document.getElementById("name").innerHTML = "<b>SNEAKER COMPANY</b>";
+
+
+document.getElementById("nameedition").innerHTML = "<b>Fall Limited Edition Sneakers</b>";
+
+document.getElementById("description").innerHTML = "These low-profile sneakers are your perfect casual wear companion.Featuring a durable rubber outer sole, they’ll withstand everything theweather can offer.";
+
+
+document.getElementById("discount").innerHTML ="<s><b>$250.00</b></s>";
+document.getElementById("desc").innerHTML ="These low-profile sneakers are your perfect <br>casual wear companion.Featuring a durable <br> rubber outer sole, they’ll withstand everything <br> theweather can offer.";
+document.getElementById("").innerHTML ="";
+//Increasing product
+function incrementValue()
+{
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    if(value<10){
+        value++;
+            document.getElementById('number').value = value;
+    }
+}
+
+//Decreasing product
+function decrementValue()
+{
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    if(value>1){
+        value--;
+            document.getElementById('number').value = value;
+    }
+
+}
+
+//hiding shopping cart
+function myFunction() {
+
+  var x = document.getElementById("myDIV");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+//side bar
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
+
+
+
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+  
